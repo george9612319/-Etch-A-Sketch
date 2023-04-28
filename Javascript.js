3a2c094427e1b9c4bd5e4888b2de17c6f042edfa
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-27 23:11:46 -0700

    complete the ETCH A SKETCH

--- Javascript.js
+++ Javascript.js
@@ -1,15 +1,48 @@
-document.addEventListener("DOMContentLoaded",drawgrid)
+//document.addEventListener("DOMContentLoaded",drawgrid);
 
 function drawgrid() {
-    const grid = document.getElementById('container'); // target the container
+    const grid = document.getElementById('container'); // target the container as grid
+    const input = document.getElementById('input'); // target the input
+    const gridcount = parseInt(input.value);
+    grid.style.gridTemplateColumns = `repeat(${gridcount}, 1fr)`; //gridcount as column and row
+    grid.style.gridTemplateRows = `repeat(${gridcount}, 1fr)`;
 
-    for(var i=0; i<256; i++) {
-        const gridItem = document.createElement('div'); 
-        gridItem.classList.add('grid-item'); // add a <div class = "grid-item"></div>
+    if (typeof gridcount === 'number' && gridcount < 49) {
+        const totalgrid = gridcount*gridcount;
+        for(var i=0; i<totalgrid; i++) {
+            const gridItem = document.createElement('div'); 
+            gridItem.classList.add('grid-item'); // add a <div class = "grid-item"></div>
 
-        const innerdiv = document.createElement('div');
-        gridItem.appendChild(innerDiv);
+            const innerdiv = document.createElement('div');
+            gridItem.appendChild(innerdiv);// add <div></div> in grid-item
 
-        grid.appendChild(gridItem);
+            gridItem.addEventListener ('mouseover', function(e) {
+                e.target.style.backgroundColor = 'black';
+            });
+
+            gridItem.addEventListener('mouseleave', function(e) {
+                e.target.style.backgroundColor = '#ccc';
+            })
+
+            grid.appendChild(gridItem);// put grid-item in grid(container)
+
+        }
     }
-}
+    else {
+        return;
+    }
+};
+
+const resetbutton = document.querySelector('button');
+resetbutton.addEventListener('click',function(e){
+    const grid = document.getElementById('container');
+    while(grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+    };
+    drawgrid();
+
+})
+
+
+
+
