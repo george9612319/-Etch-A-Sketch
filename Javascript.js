1cb8ee46e02a16d0551797801c72a14514a39aa4
george9612319 <130203573+george9612319@users.noreply.github.com>
2023-04-28 00:28:12 -0700

    add a listener for change the color immediately as I change the optio n

--- Javascript.js
+++ Javascript.js
@@ -7,18 +7,18 @@ function drawgrid() {
     grid.style.gridTemplateRows = `repeat(${gridcount}, 1fr)`;
     const option = select.options[select.selectedIndex].value;
 
-    if (typeof gridcount === 'number' && gridcount < 101) {
+    if (typeof gridcount === 'number' && gridcount < 101) { // create gud base on the iput number
         const totalgrid = gridcount*gridcount;
         for(var i=0; i<totalgrid; i++) {
             const gridItem = document.createElement('div'); 
             gridItem.classList.add('grid-item'); // add a <div class = "grid-item"></div>
 
-            gridItem.addEventListener ('mouseover', function(e) {
-                if (option === 'black'){
+            gridItem.addEventListener ('mouseover', function(e) { // grid will change color when mouseover
+                if (option === 'black'){ // whem option === black, grid show black when mouseover
                     e.target.style.backgroundColor = 'black';
-                } else if (option === 'gray' ) {
+                } else if (option === 'gray' ) { // whem option === grey, grid show grey when mouseover
                     e.target.style.backgroundColor = `rgb(224,224,224)`;
-                }else if (option === 'RGB') {
+                }else if (option === 'RGB') { // whem option === RGB, grid show random RGB when mouseover
                     var R = Math.floor(Math.random()*256);
                     var G = Math.floor(Math.random()*256);
                     var B = Math.floor(Math.random()*256);
@@ -29,7 +29,7 @@ function drawgrid() {
             )
                 
 
-            gridItem.addEventListener('mouseleave', function(e) {
+            gridItem.addEventListener('mouseleave', function(e) { // grid color will back to #ccc when mouse leave
                 e.target.style.backgroundColor = '#ccc';
             })
 
@@ -42,11 +42,11 @@ function drawgrid() {
     }
 };
 
-const resetbutton = document.querySelector('button'); // reset the grid
+const resetbutton = document.querySelector('button'); // reset the grid when click button
 resetbutton.addEventListener('click',function(e){
     const grid = document.getElementById('container');
     while(grid.firstChild) { // delete the grid
-    grid.removeChild(grid.firstChild);
+    grid.removeChild(grid.firstChild); 
     };
     drawgrid(); // recreate grids
 })
@@ -58,3 +58,10 @@ const select = document.getElementById('select'); // add three options for choos
     option.text = color;
     select.add(option);
     })
+select.addEventListener('change', function(e){ // recreate the grid as change the option of color
+    const grid = document.getElementById('container');
+    while(grid.firstChild) { // delete the grid
+    grid.removeChild(grid.firstChild); 
+    };
+    drawgrid();
+})
